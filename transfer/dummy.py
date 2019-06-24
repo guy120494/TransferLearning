70e0b276b277fbb33a25b601716d4fae4b49dbb0
Guy Smorodinsky <guy120494@gmail.com>
2019-06-24 21:23:54 +0300

    Added a dummy NN

--- transfer/dummy.py
+++ transfer/dummy.py
@@ -1,8 +1,49 @@
-from keras.models import Sequential
-from keras.layers import Dense
+# 3. Import libraries and modules
+import numpy as np
+import tensorflow as tf
 
-model = Sequential()
+NUMBER_OF_LABELS = 10
 
-model.add(Dense(units=64, activation='relu', input_dim=100))
-model.add(Dense(units=10, activation='softmax'))
+MNIST_IMAGE_SIZE = 28
 
+np.random.seed(123)  # for reproducibility
+
+
+def main():
+    fac = 0.99 / 255
+    mnist = tf.keras.datasets.mnist
+    (train_data, train_labels), (test_data, test_labels) = mnist.load_data()
+    if tf.keras.backend.image_data_format() == 'channels_first':
+        train_data = train_data.reshape(train_data.shape[0], 1, MNIST_IMAGE_SIZE, MNIST_IMAGE_SIZE)
+        test_data = test_data.reshape(test_data.shape[0], 1, MNIST_IMAGE_SIZE, MNIST_IMAGE_SIZE)
+        input_shape = (1, MNIST_IMAGE_SIZE, MNIST_IMAGE_SIZE)
+    else:
+        train_data = train_data.reshape(train_data.shape[0], MNIST_IMAGE_SIZE, MNIST_IMAGE_SIZE, 1)
+        test_data = test_data.reshape(test_data.shape[0], MNIST_IMAGE_SIZE, MNIST_IMAGE_SIZE, 1)
+        input_shape = (MNIST_IMAGE_SIZE, MNIST_IMAGE_SIZE, 1)
+
+    train_data = train_data * fac + 0.01
+    test_data = test_data * fac + 0.01
+    train_labels = tf.keras.utils.to_categorical(train_labels, num_classes=NUMBER_OF_LABELS)
+    test_labels = tf.keras.utils.to_categorical(test_labels, num_classes=NUMBER_OF_LABELS)
+
+    model = tf.keras.models.Sequential()
+    model.add(tf.keras.layers.Conv2D(filters=2, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+    model.add(tf.keras.layers.MaxPooling2D())
+    model.add(tf.keras.layers.Conv2D(filters=2, kernel_size=(3, 3), activation='relu'))
+    model.add(tf.keras.layers.Flatten())
+    model.add(tf.keras.layers.Dense(activation='relu', units=50))
+    model.add(tf.keras.layers.Dense(activation='softmax', units=10))
+
+    model.compile(loss=tf.keras.losses.categorical_crossentropy,
+                  optimizer=tf.keras.optimizers.Adadelta(), metrics=['accuracy'])
+    model.fit(x=train_data, y=train_labels, batch_size=10, epochs=5, verbose=1)
+
+    result = model.predict(test_data[0:10])
+    result = [np.argmax(r) for r in result]
+    print("predictions: " + str(result))
+    print("actual: " + str([np.argmax(l) for l in test_labels[0:10]]))
+
+
+if __name__ == '__main__':
+    main()
