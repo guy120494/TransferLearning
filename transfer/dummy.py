9809ae556950fb22dd59d730ad81b345876d7046
guy <guy120494@gmail.com>
2019-07-10 18:56:57 +0300

    rename variable

--- transfer/dummy.py
+++ transfer/dummy.py
@@ -2,9 +2,9 @@ from transfer.models.base_model_builder import build_base_model
 
 
 def main():
-    models = build_base_model()
+    base_model = build_base_model()
 
-    models.save('my-model.h5')
+    base_model.save('my-model.h5')
 
 
 if __name__ == '__main__':
