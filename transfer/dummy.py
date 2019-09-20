334ef3b551f408f38b26c56f8c4aede6bde1b932
guy <guy120494@gmail.com>
2019-09-20 17:28:15 +0300

    Fixed graphs title

--- transfer/dummy.py
+++ transfer/dummy.py
@@ -161,7 +161,7 @@ def make_smoothness_graph_for_train(i):
     plt.figure()
     plt.xlabel('number of frozen layers')
     plt.ylabel('accuracy')
-    plt.title(f'smoothness over layers')
+    plt.title(f'smoothness over layers with {i} train')
     for j in range(len(smoothness_list)):
         plt.plot(x, smoothness_list[j], color=colors[j], label=f'model {j}')
     plt.legend(loc='lower right')
@@ -181,7 +181,7 @@ def make_smoothness_graph_for_model(j):
     plt.figure()
     plt.xlabel('number of frozen layers')
     plt.ylabel('smoothness index')
-    plt.title(f'smoothness over layers')
+    plt.title(f'smoothness over layers for model {j}')
     for i in range(len(smoothness_list)):
         plt.plot(x, smoothness_list[i], color=colors[i], label=f'{trains[i]} train')
     plt.legend(loc='lower left')
